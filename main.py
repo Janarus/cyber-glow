5b01bd507192a6049a1b6c73c1149a36754d56f3
Janarus <serikzako253@gmail.com>
2026-09-12 11:44:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run()
+    return "Cyber Glow K-Beauty сайты сәтті іске қосылды!"
